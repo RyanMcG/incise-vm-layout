40060799d45e81454ffae83f02595cde94e34200
Ryan McGowan <ryan@ryanmcg.com>
2014-04-11 18:59:45 -0700

    Remove depdency on incise proper.

--- project.clj
+++ project.clj
@@ -5,7 +5,6 @@
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [incise-stefon "0.1.0"]
-                 [incise "0.3.2"]
                  [incise-base-hiccup-layouts "0.2.0"]]
   :repl-options {:init-ns incise.repl}
   :aliases {"incise" ^:pass-through-help ["run" "-m" "incise.core"]})

53f5eb5ed7e6bf08473da7f5f3710b17800553bf
Ryan McGowan <ryan@ryanmcg.com>
2014-04-11 18:50:19 -0700

    Update project name.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject com.ryanmcg/www "2014"
-  :description "My personal website (w/ rendered content on gh-pages branch)."
+(defproject com.ryanmcg/incise-vm-layout "0.1.0"
+  :description "The base layout I use for my personal website."
   :url "https://github.com/RyanMcG/ryanmcg.github.io"
   :license {:name "Creative Commons Attribution-ShareAlike 3.0 Unported License"
             :url "http://creativecommons.org/licenses/by-sa/3.0/deed.en_US"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [incise-stefon "0.1.0"]
                  [incise "0.3.2"]
                  [incise-base-hiccup-layouts "0.2.0"]]
